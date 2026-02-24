d3beb552995917610a30b875e1a0367e8f1f119b
Tymivv <43436409+Tymivv@users.noreply.github.com>
2026-02-24 14:54:21 +0200

    Create decorators.py

---
+++ decorators.py
@@ -0,0 +1,11 @@
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            return str(e)
+        except KeyError:
+            return "Contact not found."
+        except IndexError:
+            return "Not enough arguments."
+    return wrapper
