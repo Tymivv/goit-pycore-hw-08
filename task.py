a0bb1bf53c8d51b40db79034783617e834785a43
Tymivv <43436409+Tymivv@users.noreply.github.com>
2026-03-01 19:09:11 +0200

    Update task.py

--- task.py
+++ task.py
@@ -2,6 +2,21 @@ import pickle
 from collections import UserDict
 from datetime import datetime, timedelta
 
+FILENAME = "addressbook.pkl"
+
+
+def save_data(book, filename=FILENAME):
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+
+def load_data(filename=FILENAME):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()
+
 
 # ===================== DECORATOR =====================
 def input_error(func):
